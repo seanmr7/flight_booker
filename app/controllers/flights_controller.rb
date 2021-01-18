f8b5bbb177073cae6417c79da63281ec8bc05616
Sean <smriedl@gmail.com>
2021-01-18 13:40:13 -0500

    Add index search method

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,8 +2,15 @@ class FlightsController < ApplicationController
 
   def index
 
-    @flights = Flight.search( )
-    @airport_options = Airport.all.map { |a| [a.city, a.id] }
-    @date_options = Flight.all.date.map { |d| [d.]}
+    @airport_options = Airport.all.map { |a| [a.name, a.id] }
+    @date_options = Flight.all.map { |d| [d.date.strftime(%F), d.date].uniq }
+    
+    if params[:index]
+      @flights = Flight.where(
+        origin_id: params[:origin_id]
+        destination_id: params[:destination_id]
+        date: params[:date]
+      )
+    end
   end
 end
