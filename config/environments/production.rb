a498c46c5f5df6bc1d66bab1be2d0f35e9808b8a
Sean Riedl <smriedl@gmail.com>
2021-02-14 14:58:53 -0500

    Set up production file for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+   # Configure production environment to send emails using gmail
+   config.action_mailer.default_url_options = { :host => 'https://young-castle-13104.herokuapp.com/' }
+   config.action_mailer.raise_delivery_errors = true
+   config.action_mailer.delivery_method = :smtp
+   config.action_mailer.smtp_settings = {
+     address:              'smtp.gmail.com',
+     port:                 587,
+     domain:               'gmail.com',
+     user_name:            ENV["GMAIL_USERNAME"],
+     password:             ENV["GMAIL_PASSWORD"],
+     authentication:       'login',
+     enable_starttls_auto: true }
 end
